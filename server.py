1bae5837ef7714c459a172f777c3322fe43f7b47
Federico Machado <federico.m.machado@texry.com>
2017-09-14 02:11:30 -0300

    no message

--- server.py
+++ server.py
@@ -1,44 +1,26 @@
 #!/usr/bin/python3
+# -*- coding: cp1252 -*-
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+import urllib
 
-db_connect = create_engine('sqlite:///chinook.db')
+db_connect = create_engine('mssql+pyodbc:///?odbc_connect=' +
+                           urllib.quote_plus('DRIVER=FreeTDS;DSN=Urusal;Description=KP local;UID=sa;Trusted_Connection=Yes;APP=Python;WSID=FEDERICOH-PC;DATABASE=KPUrusalWS;Network=DBMSLPCN'))
 app = Flask(__name__)
 api = Api(app)
 
 
 class Employees(Resource):
     def get(self):
+        #DSN=Urusal;Description=KP local;UID=sa;Trusted_Connection=Yes;APP=Python;WSID=FEDERICOH-PC;DATABASE=KPUrusalWS;Network=DBMSLPCN
         conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
+        query = conn.execute("select * from ARTICULO") # This line performs query and returns json result
         return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
     
     def post(self):
-        conn = db_connect.connect()
-        print(request.json)
-        LastName = request.json['LastName']
-        FirstName = request.json['FirstName']
-        Title = request.json['Title']
-        ReportsTo = request.json['ReportsTo']
-        BirthDate = request.json['BirthDate']
-        HireDate = request.json['HireDate']
-        Address = request.json['Address']
-        City = request.json['City']
-        State = request.json['State']
-        Country = request.json['Country']
-        PostalCode = request.json['PostalCode']
-        Phone = request.json['Phone']
-        Fax = request.json['Fax']
-        Email = request.json['Email']
-        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-                             '{13}')".format(LastName,FirstName,Title,
-                             ReportsTo, BirthDate, HireDate, Address,
-                             City, State, Country, PostalCode, Phone, Fax,
-                             Email))
-        return {'status':'success'}
+        pass
 
     
 class Tracks(Resource):
