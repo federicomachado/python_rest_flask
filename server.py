7d32acc9594a57308ef8d72d24feb76cbd5bed6a
Federico Machado <federico.m.machado@texry.com>
2017-09-15 15:04:50 -0300

    l

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ class Tracks(Resource):
         cursor = db_connect.cursor()
         query = cursor.execute("select distinct a.PrdPesBru, a.PrdPesNet from ARTICULO a") # This line performs query and returns json result
         rows = cursor.fetchall() 
-        query = cursor.execute("select trackid, name, composer, unitprice from tracks;")
+##        query = cursor.execute("select trackid, name, composer, unitprice from tracks;")
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
 
