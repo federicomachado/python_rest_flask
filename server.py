9e1b865d660db391e9b2b7c24d06561e319a9e4a
Federico Machado <federico.m.machado@texry.com>
2017-09-15 14:57:58 -0300

    g

--- server.py
+++ server.py
@@ -17,8 +17,11 @@ class Employees(Resource):
     def get(self):
         #DSN=Urusal;Description=KP local;UID=sa;Trusted_Connection=Yes;APP=Python;WSID=FEDERICOH-PC;DATABASE=KPUrusalWS;Network=DBMSLPCN
        # conn = db_connect.connect() # connect to database
-        query = db_connect.execute("select distinct a.PrdPesBru, a.PrdPesNet from ARTICULO a") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+       cursor = db_connect.cursor()
+        query = cursor.execute("select distinct a.PrdPesBru, a.PrdPesNet from ARTICULO a") # This line performs query and returns json result
+        rows = cursor.fetchall()
+        
+        return {'employees': rows} # Fetches first column that is Employee ID
     
     def post(self):
         pass
