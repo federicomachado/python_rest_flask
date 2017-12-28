588b5760f95bda235414f069ca82dc9acda36e65
Federico Machado <federico.m.machado@texry.com>
2017-12-28 00:54:51 -0300

    Configured POST for RegistryEntry

--- server.py
+++ server.py
@@ -11,15 +11,26 @@ import urllib
 
 # pip install simplejson
 
-def initWorkerParser():
+def initRegistryParser():
     parser = reqparse.RequestParser()
-    parser.add_argument("username")
-    parser.add_argument("password")
+    parser.add_argument("order_id")
+    parser.add_argument("firstWorker")
+    parser.add_argument("secondWorker")
+    parser.add_argument("meanTime")
+    parser.add_argument("meanTimePerWorker")
+    parser.add_argument("date")
+    parser.add_argument("startTime")
+    parser.add_argument("endTime")
+    parser.add_argument("stopTime")
+    parser.add_argument("realTime")
+    parser.add_argument("timePerWorker")
+    parser.add_argument("firstWorkerDesc")
+    parser.add_argument("secondWorkerDesc")
     return parser
 
 try:
     db_connect = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};DATABASE=KPUrusalWS;SERVER=ANTILSRV\SQLEXPRESS;PORT=1433;UID=FMACHADO;PWD=Fede1234')
-    workerParser = initWorkerParser()
+    registryParser = initRegistryParser()
     app = Flask(__name__)
     cors = CORS(app, resources={"*": {"origins": "*"}})
     db_sqlite3 = sqlite3.connect("entries.db")
@@ -50,12 +61,14 @@ class ProductionTime(Resource):
     def post(self):
         pass
 
-class WorkerEntry(Resource):
+class RegistryEntry(Resource):
     def get(self):
         pass
     def post(self):
-        args = workerParser.parse_args()
-        print args
+        args = registryParser.parse_args()        
+        c = db_sqlite3.cursor()
+        c.execute("""insert into entry values(?,?,?,?,?,?,?,?,?,?,?,?,?)""",(args["order_id"],args["firstWorker"],args["secondWorker"],args["meanTime"],args["meanTimePerWorker"],args["date"],args["startTime"],args["endTime"], args["stopTime"], args["realTime"],args["timePerWorker"],args["firstWorkerDesc"],args["secondWorkerDesc"]    ))
+        db_sqlite3.commit()
         return args,201
         
 
@@ -87,13 +100,13 @@ class Worker(Resource):
             dicc = dict(zip(columns,row))        
             results.append(dicc)
         return results
-        
-        
+
+
 
 api.add_resource(ProductionTime, '/times') # Route_1
 api.add_resource(Order, '/orders') # Route_2
 api.add_resource(Worker, '/workers') # Route_3
-api.add_resource(WorkerEntry, '/entries') # Route_3
+api.add_resource(RegistryEntry, '/entries') # Route_3
 
 
 if __name__ == '__main__':
